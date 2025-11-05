598a0986c399463c1ea595cfbda9d71021a7e73e
clecio81 <55952484+clecio81@users.noreply.github.com>
2025-11-05 16:38:39 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
